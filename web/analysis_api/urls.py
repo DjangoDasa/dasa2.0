06e2c95f2dff5762c19de086546b1110afe4a369
MAX MCFARLAND <mmcfarland522@gmail.com>
2019-02-25 15:47:17 -0800

    adding chart view logic

--- web/analysis_api/urls.py
+++ web/analysis_api/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.authtoken import views
 from .views import home_view, index, RegisterApiView, UserApiView
-from .views import AnalysisApiView, AnalysisGraphApiView, UsersAllApiView, analysis_exists
+from .views import AnalysisApiView, AnalysisGraphApiView, UsersAllApiView
 
 
 urlpatterns = [
@@ -12,7 +12,6 @@ urlpatterns = [
     path('register', RegisterApiView.as_view(), name='register'),
     path('login', views.obtain_auth_token),
     path('analysis', AnalysisApiView.as_view(), name='analysis-list'),
-    path('analysis/existing', analysis_exists, name='analysis-exists'),
     path('analysis/<chart>', AnalysisGraphApiView.as_view(), name='analysis-graph'),
 #     path('charts/{graph_type}', ChartApiView.as_view(), name='chart-detail'),
 
